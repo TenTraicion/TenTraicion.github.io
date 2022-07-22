6a46a5f59e3800181a364c850793e2b24e11fc53
TenTraicion <tentraicion@gmail.com>
2022-07-22 22:05:29 +0600

    Added Form

--- js/call.js
+++ js/call.js
@@ -5,6 +5,34 @@ copy.innerText = udate.getFullYear();
 
 // Calling Contact
 const contact = document.getElementById("contact");
+const backdrop = document.getElementById("backdrop");
+const ovrl = document.getElementById("ovrl");
+const cancel = document.getElementById("exit");
+const chk = document.getElementById("chk");
+const remains = document.getElementById("count");
+
+//taking input
+const uname = document.getElementById("uname");
+const email = document.getElementById("email");
+const git = document.getElementById("git");
+const twit = document.getElementById("twit");
+const sub = document.getElementById("sub");
+const msg = document.getElementById("msg");
+const max = msg.maxLength;
+
+//creating output
+const subject = document.getElementById("subject");
+const body = document.getElementById("body");
 
 // Add Listener for Contact
- contact.addEventListener("click", contactForm);
+ contact.addEventListener("click", openConfig);
+
+ //close listener
+ cancel.addEventListener("click", closeConfig);
+backdrop.addEventListener("click", closeConfig);
+
+// checkbox
+chk.addEventListener("click", checked);
+
+// count
+msg.addEventListener("input", update);
--- js/config.js
+++ js/config.js
@@ -1,7 +1,48 @@
-// Contact
-function contactForm() {
-  let i = 1;
-  i <= 10;
-  i++
-  console.log(i);
+//opening form
+function openConfig(event) {
+  player = +event.target.dataset.player;
+  ovrl.style.display = "flex";
+  backdrop.style.display = "block";
+  console.log("Form Opened!")
+}
+
+//closing form
+function closeConfig() {
+  ovrl.style.display = "none";
+  backdrop.style.display = "none";
+  console.log("Form Closed!")
+}
+
+//checkbox
+function checked() {
+  const output = `MY Information: \n Name: ${uname.value}\n Email: ${email.value}\n GitHub Profile: https://github.com/${git.value}\n Twitter: https://twitter.com/${twit.value}`;
+  const mail = `\n\n My Message:\n ${msg.value}`;
+
+  subject.value = sub.value;
+  body.value = output + mail;
+  console.log(output + mail);
+  console.log(`Mail Send!`)
+}
+
+//count
+function update(event) {
+  const entry = event.target.value;
+    const entryLength = entry.length;
+    
+    const count = max - entryLength;
+
+    remains.textContent = count;
+
+    if(count <= 10) {
+      remains.className = "error";
+      msg.classList.add("error");
+      msg.classList.remove("warning");
+  } else if(count <= 50) {
+      remains.className = "warning"
+      msg.classList.remove("error");
+      msg.classList.add("warning");
+  } else {
+      remains.className = "pos";
+      msg.classList.remove("warning");
+  }
 }
